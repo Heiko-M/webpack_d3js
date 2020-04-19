5d291092a92a825b22871751a68f2d4b2b942f10
Heiko Müller <mue.heiko@web.de>
2020-04-19 13:29:11 +0200

    Generate separate bundles for app and plot and clean dist folder on each build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 /*eslint-env node*/
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    plot: './src/plot.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'assets/index.html',
     }),
