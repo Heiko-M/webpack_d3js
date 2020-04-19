a8b870dba02c662753820b1476ad4d762c6e4db7
Heiko Müller <mue.heiko@web.de>
2020-04-19 12:49:54 +0200

    Move data to JSON (webpack knows how to bundle json dependencies out of the box).

--- src/fetch-data.js
+++ src/fetch-data.js
@@ -1,18 +1,4 @@
 /**
  * Stub implementation of data fetching from DB.
  */
-export const fetchData = () => {
-  return [
-    { x: 0, y: 205 },
-    { x: 3, y: 267 },
-    { x: 6, y: 309 },
-    { x: 9, y: 387 },
-    { x: 12, y: 476 },
-    { x: 15, y: 577 },
-    { x: 18, y: 660 },
-    { x: 21, y: 745 },
-    { x: 24, y: 797 },
-    { x: 27, y: 832 },
-    { x: 30, y: 854 },
-  ];
-};
+export const fetchData = () => require('../data/data.json');
