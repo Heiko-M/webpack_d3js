6e938b2977abccc6c84f2559cb3ce817b20c6e46
Heiko Müller <mue.heiko@web.de>
2020-04-19 12:20:37 +0200

    Rename the project and update the README.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,21 @@ module.exports = {
     'env': {
         'browser': true,
         'commonjs': true,
-        'es6': true
+        'es6': true,
     },
     'extends': 'eslint:recommended',
     'globals': {
         'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
+        'SharedArrayBuffer': 'readonly',
     },
     'parserOptions': {
         'ecmaVersion': 2018,
-        'sourceType': 'module'
+        'sourceType': 'module',
     },
     'rules': {
         'quotes': ['warn', 'single'],
         'comma-dangle': ['warn', 'always-multiline'],
         'camelcase': ['error'],
-        'semi': ['warn', 'always']
-    }
+        'semi': ['warn', 'always'],
+    },
 };
