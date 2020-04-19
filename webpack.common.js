4c87a0f5fd2c3ec69c401ae4d7554797f5bd4687
Heiko Müller <mue.heiko@web.de>
2020-04-19 14:38:07 +0200

    Split webpack config into common, dev and prod configs.

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.js',
     plot: './src/plot.js',
@@ -12,10 +11,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
   },
   plugins: [
     new CleanWebpackPlugin(),
